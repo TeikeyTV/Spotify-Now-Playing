53ee447bf97b85d8e30f38740b18c076806ddea0
Teikey <mail.teikey@gmail.com>
2026-06-05 16:19:08 +0530

    some changes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Connected');
+
+// DOM Connector
+const mainContainer = document.querySelector('.main-container');
+
+// Web Page History Mapping
+const routes = {
+    "/welcome" : "<h1>Welcome to Spotify Now Playing</h1>",
+    "/now-playing" : "<h1>Now Playing</h1>",
+    "/Close" : "<h1>Close</h1>"
+};
+
+function submitCred() {
+    console.log('Submit Cred');
+    
+}
